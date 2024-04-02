889435c64e56a840ad6fd39ca0326291ebf3f2d0
Herman Lee <kingkonghi12@gmail.com>
2024-04-02 16:57:44 +0800

    Django

--- ShopMore/ShopMore/urls.py
+++ ShopMore/ShopMore/urls.py
@@ -4,4 +4,12 @@ from . import views
  
 urlpatterns = [
     path("", views.hello, name="hello"),
+    path('hello/', views.hello),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('product/', views.product),
+    path('cart/', views.cart),
+    path('checkout/', views.checkout),
+    path('order/', views.order),
 ]
+ 
--- ShopMore/ShopMore/views.py
+++ ShopMore/ShopMore/views.py
@@ -2,3 +2,21 @@ from django.http import HttpResponse
  
 def hello(request):
     return HttpResponse("Hello world ! ")
+
+def login(request):
+    return HttpResponse("Login page")
+
+def register(request):
+    return HttpResponse("Register page")
+
+def product(request):
+    return HttpResponse("Product page")
+
+def cart(request):
+    return HttpResponse("Cart page")
+
+def checkout(request):
+    return HttpResponse("Checkout page")
+
+def order(request):
+    return HttpResponse("Order page")
