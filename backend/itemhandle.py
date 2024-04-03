199ed256d0222d00a000eb78c99b624f2d5dbf94
kingkonghi <kingkonghi12@gmail.com>
2024-04-03 11:32:10 +0800

    itemhandle.py added online database

--- backend/itemhandle.py
+++ backend/itemhandle.py
@@ -1,21 +1,19 @@
 import mariadb
 import sys
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="csci3100",
-        host="119.246.239.30",
-        port=3306,
-        database="ShopMore"
 
-    )
+try:
+    conn = mariadb.connect(db = 'sql6696311', 
+                  user   = 'sql6696311', 
+                  passwd = 'kWjC94rLvl', 
+                  host   = 'sql6.freemysqlhosting.net',
+                  port=3306
+                  ) 
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
-print("Connected to MariaDB")
+        
 
-
-def list_item():
+def list():
     cur = conn.cursor()
     cur.execute("SELECT * FROM item")
     for(itemid, itemname, itemdesc, itemcategory, itemimage, itemprice, itemquantity, itemstatus) in cur:
@@ -23,20 +21,15 @@ def list_item():
     cur.close()
 
 def add_item():
-
     cur = conn.cursor()
     cur.execute("INSERT INTO item (name, description, category, image, price, quantity, status) VALUES ('Test Item1', 'Test Description1', 1, '', 200, 1, 1)")
     conn.commit()
     print("Item added.")
     cur.close()
 
-<<<<<<< HEAD
 def edit_item():
-=======
-def edit_item_quantity(item):
->>>>>>> 889435c64e56a840ad6fd39ca0326291ebf3f2d0
     cur = conn.cursor()
-    cur.execute("UPDATE item SET ITEMQUANTITY = ? WHERE itemID = ?", (item.quantity,item.itemid))
+    cur.execute("UPDATE item SET ITEMQUANTITY = 2 WHERE itemID = '1'")
     conn.commit()
     print("Item quantity updated.")
     cur.close()
@@ -47,9 +40,5 @@ def remove_item():
     conn.commit()
     print("Item removed.")
     cur.close()
-<<<<<<< HEAD
-add_item()
-=======
     
-list_item()
->>>>>>> 889435c64e56a840ad6fd39ca0326291ebf3f2d0
+list()
