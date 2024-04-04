836519e12257cd435a9f791f10079de6f04f8b9d
mgttkk <hoyantung25497940@gmail.com>
2024-04-04 22:09:13 +0800

    logout

--- ShopMore/ShopMore/settings.py
+++ ShopMore/ShopMore/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
 
 
 # Password validation
--- ShopMore/ShopMore/views.py
+++ ShopMore/ShopMore/views.py
@@ -4,9 +4,12 @@ from rest_framework.decorators import api_view
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
 from .register import registerfunction
 from .login import loginfunction
 from .backend.handleitem import *
+
 def hello(request):
     return HttpResponse("Hello world ! ")
 
@@ -14,6 +17,13 @@ def hello(request):
 def login(request):
     response = loginfunction(request)
     return response
+
+@api_view(["POST"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({'detail': 'Successfully logged out'}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register(request):
@@ -25,6 +35,9 @@ def register(request):
 @permission_classes([IsAuthenticated])
 def test_token(request):
     return Response("passed! for {}".format(request.user.email))
+
+def edit_info(request):
+    return HttpResponse('edit_info')
 
 
 def product(request):
