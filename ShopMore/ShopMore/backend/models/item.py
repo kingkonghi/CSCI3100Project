7b7c9b222cfd8de01b38b6866b60fa468658c8bc
mgttkk <hoyantung25497940@gmail.com>
2024-04-06 00:04:41 +0800

    Modify item name

--- ShopMore/ShopMore/backend/models/item.py
+++ ShopMore/ShopMore/backend/models/item.py
@@ -5,14 +5,14 @@ class Item(models.Model):
     itemID = models.AutoField(primary_key=True)
     itemName = models.CharField(max_length=20)
     itemDescription = models.CharField(max_length=100)
-    itemcategory = models.IntegerField()
-    itemimage = models.CharField(max_length=100)
-    itemprice = models.FloatField()
-    itemquantity = models.IntegerField()
-    itemstatus = models.IntegerField()
+    itemCategory = models.IntegerField()
+    itemImage = models.CharField(max_length=100)
+    itemPrice = models.FloatField()
+    itemQuantity = models.IntegerField()
+    itemStatus = models.IntegerField()
 
     def __repr__(self):
-        return f"Item(ITERID={self.itemID}, ITEMNAME={self.itemName}, ITEMDESC={self.itemDescription}, ITEMCATEGORY={self.itemcategory}, ITEMIMAGE={self.itemimage}, ITEMPRICE={self.itemprice}, ITEMQUANTITY={self.itemquantity}, ITEMSTATUS={self.itemstatus})"
+        return f"Item(ITERID={self.itemID}, ITEMNAME={self.itemName}, ITEMDESC={self.itemDescription}, ITEMCATEGORY={self.itemCategory}, ITEMIMAGE={self.itemImage}, ITEMPRICE={self.itemPrice}, ITEMQUANTITY={self.itemQuantity}, ITEMSTATUS={self.itemStatus})"
 
     class Meta:
         db_table = 'item'
