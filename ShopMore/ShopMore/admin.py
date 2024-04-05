756b1b1c8f5441f25fa99ef5fe509d0eb90cdf0f
mgttkk <hoyantung25497940@gmail.com>
2024-04-06 00:54:19 +0800

    admin register item

--- ShopMore/ShopMore/admin.py
+++ ShopMore/ShopMore/admin.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 from backend.models.item import Item
 
-
+@admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['itemID', 'itemName', 'itemDescription', 'itemCategory', 'itemImage', 'itemPrice', 'itemQuantity', 'itemStatus']
     list_filter = ['itemCategory', 'itemStatus']
