dbb8c16f88933b27400ce16745b16a2bb1b5b306
Sam Lui <luichaksum0705@email.com>
2024-04-06 22:57:49 +0800

    user profile page

--- ShopMore/react-app/src/pages/User.js
+++ ShopMore/react-app/src/pages/User.js
@@ -3,12 +3,12 @@
 import "../index.scss"
 import * as React from 'react';
 import {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import { Card, Container, Row, Col, ListGroup, Form, FormControl, Button, InputGroup, Spinner } from 'react-bootstrap';
 
 
 const User = () => {
-    const navigate = useNavigate();
     const [formValues, setFormValues] = useState({
       username: 'givemelenggradepls',
       fullname: 'Joe Go',
@@ -16,12 +16,20 @@ const User = () => {
       contactno: '',
       address: '',
       password: '',
-      creditcard: '',
-      cardholdername: '',
-      cardnumber: '',
-      expirydate: '',
-      paymenttype: 'debit',
     });
+
+    const [paymentMethod, setPaymentMethod] = useState('');
+
+    const renderPaymentInterface = () => {
+      switch (paymentMethod) {
+          case 'Card':
+              return <CardPayment />;
+          case 'PayPal':
+              return <PayPalPayment />;
+          default:
+              return <></>;
+      }
+  };
   
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -108,20 +116,59 @@ const User = () => {
                     </p>
                     <br/>
                     <p id="row">
-                        <label class="cell label">Change Password</label>
-                        <a href="#" onclick="document.getElementById('changepw').style.display='block'">Set a new password</a>
+                      <label className="cell label">Change Password</label>
+                      <Popup
+                        trigger={
+                          <p className='nav-link' style={{ color: "Highlight", textDecoration: "underline" }}>Set a new password</p>
+                        }
+                        modal
+                        nested
+                      >
+                        {(close) => (
+                          <div className="changepw">
+                            <div className='content' style={{ maxWidth: '400px', margin: '0 auto' }}>
+                              <h3 style={{ marginBottom: '20px', textAlign: 'center' }}>Set New Password</h3>
+                              <form id="changepw" style={{ display: 'table' }}>
+                                <p style={{ display: 'table-row' }}>
+                                  <label htmlFor="oldPassword" className="cell label" 
+                                  style={{ display: 'table-cell', paddingRight: '20px' }}>Original Password</label>
+                                  <input type="password" id="oldPassword" name="oldPassword" required style={{ display: 'table-cell' }} />
+                                </p>
+                                <br/>
+                                <p style={{ display: 'table-row' }}>
+                                  <label htmlFor="newPassword" className="cell label" 
+                                  style={{ display: 'table-cell', paddingRight: '20px' }}>New Password</label>
+                                  <input type="password" id="newPassword" name="newPassword" required style={{ display: 'table-cell' }} />
+                                </p>
+                                <br/>
+                                <p style={{ display: 'table-row' }}>
+                                  <label htmlFor="confirmPassword" className="cell label" 
+                                  style={{ display: 'table-cell', paddingRight: '20px' }}>Confirm Password</label>
+                                  <input type="password" id="confirmPassword" name="confirmPassword" required style={{ display: 'table-cell' }} />
+                                </p>
+                              </form>
+                              <div style={{ textAlign: "center" }}>
+                                <button onClick={() => close()} style={{ marginRight: '30px', marginTop: '20px' }}>Cancel</button>
+                                <button type="submit" form="changepw">Change</button>
+                              </div>
+                            </div>
+                            </div>
+                        )}
+                      </Popup>
                     </p>
+
                     <br/>
                     <Form className="my-3">
                         <Form.Group controlId="paymentMethodSelect">
                         <Form.Label><strong>Default Payment Method</strong></Form.Label>
-                        <Form.Select>
+                        <Form.Select value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
                             <option value="">Choose...</option>
                             <option value="Card">Credit Card / Debit Card</option>
                             <option value="PayPal">PayPal</option>
                         </Form.Select>
                         </Form.Group>
                     </Form>
+                    {renderPaymentInterface()}
                     <br />
                     <p id="ButtonContainer">
                         <input type="submit" value="Cancel"/>
@@ -134,5 +181,84 @@ const User = () => {
       </>
     );
   };
+
+  const CardPayment = () => {
+    const [cardDetails, setCardDetails] = useState({
+        cardNumber: '',
+        cardHolderName: '',
+        expiryMonth: '',
+        expiryYear: '',
+        cvv: '',
+    });
+
+    const handleChange = (e) => {
+        setCardDetails({ ...cardDetails, [e.target.name]: e.target.value });
+    };
+    
+    return (
+          <div className="default-card">
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label>Card Number</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="cardNumber"
+                        placeholder="Card Number"
+                        value={cardDetails.cardNumber}
+                        onChange={handleChange}
+                        required
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Cardholder Name</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="cardHolderName"
+                        placeholder="Cardholder name"
+                        value={cardDetails.cardHolderName}
+                        onChange={handleChange}
+                        required
+                        />
+                    </Form.Group>
+
+                    <Form.Label>Expiry Date</Form.Label>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                        placeholder="MM"
+                        name="expiryMonth"
+                        value={cardDetails.expiryMonth}
+                        onChange={handleChange}
+                        style={{ maxWidth: '70px' }}
+                        required
+                        />
+                        <FormControl
+                        placeholder="YY"
+                        name="expiryYear"
+                        value={cardDetails.expiryYear}
+                        onChange={handleChange}
+                        style={{ maxWidth: '70px' }}
+                        required
+                        />
+                        <FormControl
+                        placeholder="CVV"
+                        name="cvv"
+                        value={cardDetails.cvv}
+                        onChange={handleChange}
+                        style={{ maxWidth: '70px' }}
+                        required
+                        />
+                    </InputGroup>
+          </div>
+    );
+};
+
+const PayPalPayment = () => {
+  return (
+      <div>
+      <p>Default PayPal Payment</p>
+      </div>
+  );
+};
 
 export default User;
