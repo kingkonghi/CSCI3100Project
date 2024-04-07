63f69fe8bfacfea3cc876d3eda31b0891d3bd233
mgttkk <hoyantung25497940@gmail.com>
2024-04-07 18:36:37 +0800

    update handleuser

--- ShopMore/ShopMore/backend/handleuser.py
+++ ShopMore/ShopMore/backend/handleuser.py
@@ -1,22 +1,39 @@
-from .models.user import *
+from .models.user import User as UserList
 from .models.favoritelist import *
 from .models.cart import *
-def list_user():
-    rows = User.objects.all()
-    print(rows)
-    return rows
+from django.contrib.auth.models import User
+
+def list_user_info(user):
+    user_info = UserList.objects.filter(user_id=user.id)
+    return user_info
 
 def add_user(user):
-    user_count = User.objects.count()
-    user1 = User(userID=user_count+1,username=user.username, accountType=user.accountType, password=user.password, email=user.email, profilePhoto=user.profilePhoto, address=user.address)
+    user_count = UserList.objects.count()
+    user1 = UserList(user_id=user_count+1,username=user.username, accountType=user.accountType, password=user.password, email=user.email, profilePhoto=user.profilePhoto, address=user.address)
     user1.save()
+
+    try: #when edit/create user in table userlist through admin, auth_user will also be edited/created 
+        auth_user = User.objects.get(id=user1.user_id)
+    except User.DoesNotExist:
+        auth_user = User(id=user1.user_id)
+    auth_user.username = user1.username
+    auth_user.set_password(user1.password)
+    auth_user.email = user1.email
+    auth_user.save()
     print(f"User {user.username} added.")
 
 def edit_user(user):
-    User.objects.filter(userID=user.userID).update(username=user.username, accountType=user.accountType, password=user.password, email=user.email, profilePhoto=user.profilePhoto, address=user.address)
-    
+    UserList.objects.filter(user_id=user.id).update(username=user.username, accountType=user.accountType, password=user.password, email=user.email, profilePhoto=user.profilePhoto, address=user.address)
+
+    auth_user = User.objects.get(id=user.id)
+    auth_user.username = user.username
+    auth_user.set_password(user.password)
+    auth_user.email = user.email
+    auth_user.save()
+
+
 def delete_user(userID):
-    User.objects.filter(userID=userID).delete()
-    FavoriteList.objects.filter(userID=userID).delete()
+    UserList.objects.filter(user_id=userID).delete()
+    FavoriteList.objects.filter(userid=userID).delete()
     cart.objects.filter(userID=userID).delete()
     print("User deleted.")
