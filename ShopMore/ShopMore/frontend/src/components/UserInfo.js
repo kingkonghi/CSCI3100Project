154a24837eed76778ab25b42d7951deaf45779de
Sam Lui <luichaksum0705@email.com>
2024-04-10 13:22:55 +0800

    userinfo.js logic

--- ShopMore/ShopMore/frontend/src/components/UserInfo.js
+++ ShopMore/ShopMore/frontend/src/components/UserInfo.js
@@ -1,33 +1,59 @@
 // Student Name : Lee Ho Kan, Leung Tsz Chung, Lee Kong Yau, Lui Chak Sum, Ho Yan Tung
 // Student ID : 1155157376, 1155141896, 1155149600, 1155158054, 1155176122
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { MdOutlineKeyboardArrowUp } from "react-icons/md";
-import { FaRegUserCircle } from "react-icons/fa";
 
+const UserInfo = () => {
+  const username = localStorage.getItem('username');
+  const navigate = useNavigate();
+  const login = username !== null;
 
+  const handleLogout = (event) => {
+    event.preventDefault();
 
-// import React components
-import "../index.scss"
+    localStorage.removeItem('token');
+    localStorage.removeItem('userid');
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    localStorage.removeItem('email');
+    alert('Logout successful');
+    // Redirect after successful logout
+    navigate('/login');
+  };
 
-const UserInfo = ({login}) => {
+  // Display root element
+  return (
+    <>
+      <div id="userInfo">
+        {login ? (
+          <div>
+            <p id="intro">You are logged in as </p>
+            <p id="name">{username}</p>
+          </div>
+        ) : (
+          <div>
+            <p id="name-warn">You are not logged in.</p>
+            <button type="text" onClick={() => navigate('/login')}>Log in</button>
+          </div>
+        )}
+        {login ? (
+          <div>
+            <hr />
+            <button type="text" onClick={() => navigate('/user')}>
+              View personal info
+            </button>
+            <hr />
+            <button type="text" onClick={handleLogout}>
+              Log out
+            </button>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </>
+  );
+};
 
-    const username = "givemelenggradepls"
-    const navigate = useNavigate();
-    //display root element
-    return (
-        <>
-            <div id='userInfo'>
-                {login?<div><p id='intro'>You are logged in as </p><p id='name'>{username}</p></div>:<div><p id='name-warn'>You are not logged in.</p><button type='text'>Log in</button></div>}
-                {login? <div>
-                            <hr/>
-                            <button type='text' onClick={() => navigate('/user')}>View personal info</button>
-                            <hr/>
-                            <button type='text' onClick={() => navigate('/login')}>Log out</button>
-                        </div>:<div></div>}
-            </div>
-        </>
-    )
-}
-export default UserInfo
+export default UserInfo;
