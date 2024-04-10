baf525e340649e16aec9abc3020ba201492e839f
Sam Lui <luichaksum0705@email.com>
2024-04-11 03:21:22 +0800

    orderlist

--- ShopMore/ShopMore/frontend/src/pages/Orderlist.js
+++ ShopMore/ShopMore/frontend/src/pages/Orderlist.js
@@ -2,22 +2,46 @@
 // Student ID : 1155157376, 1155141896, 1155149600, 1155158054, 1155176122
 import "../index.scss"
 import * as React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 const Orderlist = () => {
-    const [orderlist, setorderlist] = useState([
-      { oid: "202403301", date: "2024-03-30", totalprice: 40 , status: "Completed"},
-      { oid: "202403311", date: "2024-03-31", totalprice: 2000 , status: "Delivering"},
-      { oid: "202403312", date: "2024-03-31", totalprice: 2020 , status: "Packing"}
-    ]);
-  
-    const updateOrder = (updatedOrder) => {
-      setorderlist(updatedOrder);
+  const [orderlist, setOrderlist] = useState([]);
+
+  useEffect(() => {
+    const getOrder = async (userID) => {
+      try {
+        const url = `http://127.0.0.1:8000/order/${userID}`;
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorage.getItem("token")
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data.message);
+          setOrderlist(data.message); 
+        } else {
+          console.error('Error:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
     };
-  
-    return (
-    <div id="orderlist"> 
+
+    const userID = localStorage.getItem("userid");
+    getOrder(userID);
+  }, []);
+
+  const updateOrder = (updatedOrder) => {
+    setOrderlist(updatedOrder);
+  };
+
+  return (
+    <div id="orderlist">
       <div className="container">
         <div className="row">
           <h4>Your Order:</h4>
@@ -32,34 +56,39 @@ const Orderlist = () => {
               </tr>
             </thead>
             <tbody>
-              {orderlist.map((file, index) => (
-                <TRO i={index} key={index} data={orderlist} updateOrder={updateOrder}/>
-              ))}
+              {orderlist.length > 0 ? ( // Add conditional check before mapping over orderlist
+                orderlist.map((order, index) => (
+                  <TRO i={index} key={index} order={order} updateOrder={updateOrder} />
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="5">No orders found.</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
       </div>
-      </div>
+    </div>
+  );
+};
+
+class TRO extends React.Component {
+  render() {
+    const { order } = this.props; // Remove the index from the object destructuring
+    const link = '/order/' + order.orderID;
+    return (
+      <tr>
+        <td>{order.orderID}</td>
+        <td>{order.orderDate}</td>
+        <td>{order.orderTotal}</td>
+        <td>{order.orderStatus === 0 ? 'Packing' : 'Delivered'}</td>
+        <td>
+          <Link to={link}>Details</Link>
+        </td>
+      </tr>
     );
-  };
+  }
+}
 
 export default Orderlist;
-
-    class TRO extends React.Component {
-    
-      render() {
-        let i = this.props.i;
-        let link = '/order/' + this.props.data[i].oid;
-        return (
-          <tr>
-            <td>{this.props.data[i].oid}</td>
-            <td>{this.props.data[i].date}</td>
-            <td>{this.props.data[i].totalprice}</td>
-            <td>{this.props.data[i].status}</td>
-            <td>
-                <Link to={link}> Details </Link>
-            </td>
-          </tr>
-        );
-      };
-    }
